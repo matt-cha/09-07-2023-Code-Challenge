b2043c47c4645824e089d6b31986678654b97b86
Matt Cha <mcha002@ucr.edu>
2023-09-08 11:58:40 -0700

    Added base structure for hero image and now playing plus tomorrow's schedule

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-$(document).ready(function () {
-    // Use jQuery's ajax() method to fetch data from TVmaze API
+$(document).ready(function() {
+    // Function to format the date for tomorrow in 'YYYY-MM-DD' format
+    function getTomorrowDate() {
+        const today = new Date();
+        const tomorrow = new Date(today);
+        tomorrow.setDate(today.getDate() + 1);
+        const year = tomorrow.getFullYear();
+        let month = tomorrow.getMonth() + 1;
+        let day = tomorrow.getDate();
+        month = month < 10 ? '0' + month : month;
+        day = day < 10 ? '0' + day : day;
+        return `${year}-${month}-${day}`;
+    }
+
+
     $.ajax({
-        url: 'https://api.tvmaze.com/schedule',
+        url: 'https://api.tvmaze.com/schedule?country=US',
         method: 'GET',
-        success: function (data) {
-            // Process and populate the schedule section
-            // Ensure proper handling of the data and create a horizontal scrolling UI
+        data: { date: getTomorrowDate() },
+        success: function(data) {
+            // Process the data and create the schedule UI
+            for (let i = 0; i < 24; i++) {
+                const show = data[i]; // Access show data
+
+                // Create HTML elements for each show and append them to the schedule container
+                const showElement = $('<div class="show">');
+                showElement.append(`<img src="${show.show.image.medium}" alt="${show.show.name}">`);
+                $('.tomorrows-schedule').append(showElement);
+            }
         },
-        error: function (error) {
-            console.log('Error:', error);
+        error: function(error) {
+            console.log('Error fetching schedule data: ', error);
         }
     });
 });
